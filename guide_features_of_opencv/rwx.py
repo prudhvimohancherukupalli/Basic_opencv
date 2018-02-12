71072920866d0e25dde4a3678980c1192d03a035
prudhvi <prudhvimohan8@gmail.com>
2018-02-12 04:48:02 +0000

    Update and rename rwx.py  to rwx.py

--- guide_features_of_opencv/rwx.py
+++ guide_features_of_opencv/rwx.py
@@ -2,7 +2,8 @@
 
 
 import cv2
-import numpy as np# used to read the image. in gray formate
+import numpy as np
+img=cv2.imread('saa.jpg',0)#used to read image in gray scale
 cv2.imwrite('prudhvi.png',img)
 cv2.imshow('image',img)
 cv2.waitKey(0)
